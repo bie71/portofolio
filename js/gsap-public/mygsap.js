c5140154803e54e5627f9b5e564b23846fbe69ff
bie71 <bie77.habibi@gmail.com>
2022-05-11 22:45:25 +0700

    fix

--- js/gsap-public/mygsap.js
+++ js/gsap-public/mygsap.js
@@ -2,10 +2,10 @@ gsap.to("#habibi", { duration: 1.5, delay: 1.5, text: "Habibi" });
 
 gsap.from("#skill", { duration: 1.5, delay: 1.5, x: -200, opacity: 0, ease: "back" });
 
-gsap.from(".imghabibi", {
-  duration: 0.4,
+gsap.from("#imghabibi", {
+  duration: 1.5,
   delay: 1,
-  scale: 0.01,
   opacity: 0,
-  ease: "back.out(1, 0.3)",
+  y: -100,
+  ease: "elastic",
 });
